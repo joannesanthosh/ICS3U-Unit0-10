ade2989f1158527e7c2be535b941f21b61654a0c
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-10-14 03:41:08 +0000

    init

---
+++ program.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Joanne Santhosh
+# Created on: Oct 2022
+# This is the 0-10 program
+
+import math
+
+
+def main():
+    # This function adds 0.1 and 0.2 together
+
+    print("0.1 + 0.2 is {}.".format(0.1 + 0.2))
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
